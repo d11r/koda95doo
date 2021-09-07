36f9c65e79f9156fbcb4a769cc0403f57d75a59f
Dragos Strugar <itsdragos@gmail.com>
2021-09-07 20:27:28 +0200

    fix locale

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
   site_name: 'KODA95',
   title: 'KODA95 | Posao u EU - Apliciraj Besplatno',
   description: 'Najlakši put do posla u EU za sve profesije',
-  locale: 'sr_Latn_RS',
+  locale: 'sr',
 };
