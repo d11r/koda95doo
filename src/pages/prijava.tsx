5f3a923612416fb0cc6ae14c5a96c207a8210f2f
Dragos Strugar <itsdragos@gmail.com>
2021-09-09 12:57:26 +0200

    meta info for application form

--- src/pages/prijava.tsx
+++ src/pages/prijava.tsx
@@ -1,5 +1,15 @@
+import { Meta } from '../layout/Meta';
 import { Application } from '../templates/Application';
+import { AppConfig } from '../utils/AppConfig';
 
-const Prijava = () => <Application />;
+const Prijava = () => (
+  <>
+    <Meta
+      title={AppConfig.application_title}
+      description={AppConfig.application_descripion}
+    />
+    <Application />
+  </>
+);
 
 export default Prijava;
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -3,4 +3,6 @@ export const AppConfig = {
   title: 'KODA95 | Posao u EU - Apliciraj Besplatno',
   description: 'Najlakši put do posla u EU za sve profesije',
   locale: 'sr',
+  application_title: 'KODA95 | EU Posao | Prijava',
+  application_descripion: 'Besplatno je prijavi za EU poslove u struci',
 };
