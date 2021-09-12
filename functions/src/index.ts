bfe481024247c4fb734d49cb726c001b0c246f14
Dragos Strugar <itsdragos@gmail.com>
2021-09-12 22:59:02 +0200

    firebase functions email sendgrid

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+import * as sgMail from '@sendgrid/mail';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp();
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
+sgMail.setApiKey(API_KEY);
+
+export const welcomeEmail = functions.firestore
+  .document('job_applications/{applicationId}')
+  .onCreate((snap) => {
+    const newValue = snap.data();
+    const message = {
+      to: newValue.email,
+      from: 'posao@koda95doo.com',
+      templateId: TEMPLATE_ID,
+    };
+    return sgMail.send(message);
+  });
