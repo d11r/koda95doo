bb42851d44fd174bdf4203ea46f1e8c6538b19a1
Dragos Strugar <itsdragos@gmail.com>
2021-09-13 18:12:46 +0200

    fb pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
+
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
 import { resetIdCounter } from 'react-tabs';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-phone-input-2/lib/style.css';
@@ -6,6 +9,30 @@ import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   resetIdCounter();
+  const mountedRef = React.useRef(false);
+  React.useEffect(() => {
+    if (mountedRef.current === false) {
+      import('react-facebook-pixel')
+        .then((x) => x.default)
+        .then((ReactPixel) => {
+          if (process.env.NEXT_PUBLIC_PIXEL_ID) {
+            ReactPixel.init(process.env.NEXT_PUBLIC_PIXEL_ID);
+            ReactPixel.pageView();
+
+            Router.events.on('routeChangeComplete', (url: string) => {
+              if (url === '/prijava/') {
+                ReactPixel.trackCustom('InitiateSubmitApplication', null);
+              } else if (url === '/hvala/') {
+                ReactPixel.track('SubmitApplication', null);
+              }
+              ReactPixel.pageView();
+            });
+          }
+        });
+      mountedRef.current = true;
+    }
+  });
+
   return <Component {...pageProps} />;
 };
 
