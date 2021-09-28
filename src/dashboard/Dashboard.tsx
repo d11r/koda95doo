e6dadab3f699f18e258110ea9e054d173c39ac38
Dragos Strugar <itsdragos@gmail.com>
2021-09-28 12:32:04 +0200

    dashboard table changes

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { QuerySnapshot, DocumentData } from 'firebase/firestore';
 
 import { Spinner } from '../application/Spinner';
@@ -10,52 +11,99 @@ const UserTable = ({
 }: {
   submissions: QuerySnapshot<DocumentData> | null;
 }) => {
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Ime', sortable: false, width: 160 },
+    {
+      field: 'education',
+      headerName: 'Obrazovanje',
+      sortable: true,
+      width: 160,
+    },
+    {
+      field: 'occupation',
+      headerName: 'Zanimanje',
+      sortable: true,
+      width: 200,
+    },
+    {
+      field: 'yoe',
+      headerName: 'Iskustvo',
+      sortable: true,
+      width: 120,
+      disableColumnMenu: true,
+      type: 'number',
+    },
+    {
+      field: 'currentlyEmployed',
+      headerName: 'Zaposlen',
+      width: 120,
+      type: 'boolean',
+      disableColumnMenu: true,
+    },
+    {
+      field: 'languages',
+      headerName: 'Jezici',
+      width: 160,
+      sortable: false,
+    },
+    {
+      field: 'dateOfBirth',
+      headerName: 'Datum rođ.',
+      sortable: true,
+      width: 160,
+      type: 'date',
+    },
+    {
+      field: 'exp',
+      headerName: 'Opis poslova',
+      width: 320,
+      sortable: false,
+      type: 'text',
+    },
+  ];
+
+  const rows =
+    submissions != null
+      ? submissions?.docs.map((submission, index) => {
+          const doc = submission.data();
+          return {
+            // personal info
+            id: index + 1,
+            name: `${doc.name} ${doc.surname}`,
+            email: doc.email,
+            phone: doc.phone,
+            dateOfBirth: new Date(doc.date_of_birth.seconds * 1000),
+            // Education and work
+            education: doc.education,
+            occupation: doc.occupation,
+            yoe: doc.years_of_experience,
+            licences: doc.licences,
+            exp: doc['previous-job-desc'],
+            // Availability and requests
+            availableNow: doc.immediately_available,
+            requests: doc.special_requests,
+            currentlyEmployed: doc.currently_employed,
+            // Languages
+            languages: Object.keys(doc.languages)
+              .filter((x) => doc.languages[x].value !== 'Bez znanja')
+              .join(', '),
+          };
+        })
+      : [];
+
   return (
-    <div className="overflow-x-auto">
-      <table className="table w-full table-compact">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Ime i Prezime</th>
-            <th>Zanimanje</th>
-            <th>Dostupan odmah?</th>
-            <th>God. iskustva</th>
-            <th>Email</th>
-            <th>Telefon</th>
-            <th>Jezici</th>
-            <th>Državljanstvo</th>
-            <th>Iskustvo</th>
-          </tr>
-        </thead>
-        <tbody>
-          {submissions?.docs.map((doc, idx) => {
-            const data = doc.data();
-            const languages = Object.keys(data.languages);
-            const knownLanguages = languages
-              .map((key) => {
-                if (data.languages[key].value !== 'Bez znanja') {
-                  return data.languages[key].label;
-                }
-                return null;
-              })
-              .filter((x) => !!x);
-            return (
-              <tr key={doc.id}>
-                <th>{idx + 1}</th>
-                <td>{`${data.name} ${data.surname}`}</td>
-                <td>{data.occupation}</td>
-                <td>{data.immediately_available ? 'Da' : 'Ne'}</td>
-                <td>{data.years_of_experience}</td>
-                <td>{data.email}</td>
-                <td>{data.phone}</td>
-                <td>{knownLanguages.join(', ')}</td>
-                <td>{data.citizenship.join(', ')}</td>
-                <td>{data['previous-job-desc']}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="w-full">
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        autoHeight={true}
+        disableSelectionOnClick
+        className="w-full"
+        autoPageSize={true}
+        density="compact"
+        loading={submissions == null || submissions.size < 1}
+      />
     </div>
   );
 };
@@ -66,14 +114,14 @@ const NavHeader = () => {
       <div className="flex-1 px-2 mx-2">
         <span className="text-lg font-bold">KODA95doo Admin</span>
       </div>
-      <div className="flex-none"></div>
+      <div className="flex-none">{/* right-hand size menu here */}</div>
     </div>
   );
 };
 
 const StatList = ({ allCount }: { allCount: number | bigint }) => {
   return (
-    <div className="shadow stats mb-8">
+    <div className="shadow stats mb-4">
       <div className="stat">
         <div className="stat-title">Ukupno prijava</div>
         <div className="stat-value text-success">
@@ -101,7 +149,7 @@ const Dashboard = () => {
   return (
     <>
       <NavHeader />
-      <div className="container mx-auto px-4 mt-8">
+      <div className="container mx-auto px-2 md:px-0 mt-4">
         <StatList allCount={getSubmissionCount(allSubmissions)} />
         <UserTable submissions={allSubmissions} />
       </div>
